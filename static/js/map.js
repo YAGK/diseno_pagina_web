5d98e295af714ec32086d19149e2b6ae5abcc6e5
KevinGg1008 <Kevinguegra@gmail.com>
2022-09-06 01:48:15 -0500

    Todo junto prueba funcionamiento finaaaal-Ya se mueve

--- static/js/map.js
+++ static/js/map.js
@@ -1,4 +1,7 @@
-var map = L.map('map').setView([Lat, Lon], 13);
+
+
+
+var map = L.map('map').setView([parseFloat(Lati), parseFloat(Longi)], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -9,5 +12,10 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
-
-var marker = L.marker([Lat, Lon]).addTo(map);
+function mapita  (){
+console.log(Lati,Longi); 
+var marker = L.marker([parseFloat(Lati), parseFloat(Longi)]).addTo(map); //Añade marcadores
+map.flyTo([parseFloat(Lati), parseFloat(Longi)], 14)
+}
+mapita()
+setInterval(mapita, 4900)
