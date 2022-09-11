641c7e4924c6c81a1911368abbc7119331988e88
KevinGg1008 <Kevinguegra@gmail.com>
2022-09-11 18:37:07 -0500

    Pull cambios en el mapa (zoom)

--- static/js/map.js
+++ static/js/map.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> 18e544775af7f64cb209a35d10194c7f225f411f
 var map = L.map('map').setView([parseFloat(Lati), parseFloat(Longi)], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -18,11 +12,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 function mapita  (){
 console.log(Lati,Longi); 
 var marker = L.marker([parseFloat(Lati), parseFloat(Longi)]).addTo(map); //Añade marcadores
-<<<<<<< HEAD
-map.flyTo([parseFloat(Lati), parseFloat(Longi)], 14)
-=======
 map.flyTo([parseFloat(Lati), parseFloat(Longi)])
->>>>>>> 18e544775af7f64cb209a35d10194c7f225f411f
 }
 mapita()
 setInterval(mapita, 4900)
