eecb2490d701c5cdf06187f937dfc11a5a93cf6c
Danielsaggar <yesid.p.g06@gmail.com>
2022-09-11 18:36:50 -0500

    Edición de Map.JS

--- static/js/map.js
+++ static/js/map.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> 18e544775af7f64cb209a35d10194c7f225f411f
 var map = L.map('map').setView([parseFloat(Lati), parseFloat(Longi)], 13);
-
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
@@ -13,11 +6,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 function CamMap  (){
 console.log(Lati,Longi); 
 var marker = L.marker([parseFloat(Lati), parseFloat(Longi)]).addTo(map); //Añade marcadores
-<<<<<<< HEAD
-map.flyTo([parseFloat(Lati), parseFloat(Longi)], 14)
-=======
 map.flyTo([parseFloat(Lati), parseFloat(Longi)])
->>>>>>> 18e544775af7f64cb209a35d10194c7f225f411f
 }
-mapita()
+CamMap()
 setInterval(CamMap, 4900)
