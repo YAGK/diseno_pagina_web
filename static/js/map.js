ba3bcfb4c5c60182c1e8c45c5954c735925d130d
Danielsaggar <yesid.p.g06@gmail.com>
2022-09-12 11:52:18 -0500

    Pruebas #9-evitar acumulación

--- static/js/map.js
+++ static/js/map.js
@@ -11,11 +11,10 @@ var marker = L.marker([parseFloat(Lati), parseFloat(Longi)]).addTo(map); //Añad
 map.flyTo([parseFloat(Lati), parseFloat(Longi)])
 PreLa=Lati;
 PreLo=Longi;
+}
 if(parseFloat(Lati)!=parseFloat(PreLa)){
     console.log("Pre", PreLa, PreLo)   
         CamMap()
         setInterval(CamMap, 4900)
     }
     clearInterval();
-}
-
