c6de9c087f05ed795a193a46e17e363c7fe57b13
Danielsaggar <yesid.p.g06@gmail.com>
2022-09-12 11:42:42 -0500

    Pruebas #6-evitar acumulación

--- static/js/map.js
+++ static/js/map.js
@@ -1,11 +1,11 @@
 var map = L.map('map').setView([parseFloat(Lati), parseFloat(Longi)], 13);
-console.log("Se repite"); 
 PreLa=0, PreLo=0;
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
 function CamMap  (){
+console.log("Pre", PreLa, PreLo)
 console.log(Lati,Longi); 
 var marker = L.marker([parseFloat(Lati), parseFloat(Longi)]).addTo(map); //Añade marcadores
 map.flyTo([parseFloat(Lati), parseFloat(Longi)])
@@ -16,6 +16,4 @@ console.log("Pre", PreLa, PreLo)
     setInterval(CamMap, 4900)
 PreLa=Lati;
 PreLo=Longi;
-}else{
-    clearInterval(CamMap)
-}
+}clearInterval(CamMap)
