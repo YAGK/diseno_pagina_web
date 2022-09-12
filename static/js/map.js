76e2186b8d911e8c39ab5031999a7bfc9ab98b73
Danielsaggar <yesid.p.g06@gmail.com>
2022-09-12 10:57:37 -0500

    Prueba correción de acumulación de puntos

--- static/js/map.js
+++ static/js/map.js
@@ -1,4 +1,5 @@
 var map = L.map('map').setView([parseFloat(Lati), parseFloat(Longi)], 13);
+PreLa=0, PreLo=0;
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
@@ -8,5 +9,11 @@ console.log(Lati,Longi);
 var marker = L.marker([parseFloat(Lati), parseFloat(Longi)]).addTo(map); //Añade marcadores
 map.flyTo([parseFloat(Lati), parseFloat(Longi)])
 }
+if(parseFloat(Lati)!=parseFloat(PreLa)&&parseFloat(Longi)!=parseFloat(PreLo)){
+    
 CamMap()
 setInterval(CamMap, 4900)
+PreLa=Lati;
+PreLo=Longi;
+
+}
