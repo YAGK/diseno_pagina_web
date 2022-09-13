f527f91c830784bc36f413d7971d2f5ee84c8dd9
Danielsaggar <yesid.p.g06@gmail.com>
2022-09-12 21:31:28 -0500

    Pruebas #8-PoliLinea

--- static/js/map.js
+++ static/js/map.js
@@ -3,6 +3,7 @@ var polylinePoints;
 var polyline;
 PreLa=Lati;
 PreLo=Longi;
+look=1;
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
@@ -11,19 +12,22 @@ map.flyTo([parseFloat(Lati), parseFloat(Longi)])
 function CamMap  (){
 console.log("Pre", PreLa, PreLo)
 console.log(Lati,Longi);
-if(parseFloat(Lati)!=parseFloat(PreLa)&&parseFloat(Longi)!=parseFloat(PreLo)){
-    var marker = L.marker([parseFloat(Lati), parseFloat(Longi)]).addTo(map) //Añade marcadores
+if(parseFloat(PreLa)!=0.0&&parseFloat(PreLo)!=0.0){
     document.addEventListener("visibilitychange", () => {
-        if (document.visibilityState === 'hidden') {
-            map.flyTo([parseFloat(Lati), parseFloat(Longi)])
+        if (document.visibilityState === 'hidden'||look>1) {
+            map.flyTo([parseFloat(Lati), parseFloat(Longi), 16])
+            look=-1;
         }});
-    //map.flyTo([parseFloat(Lati), parseFloat(Longi)])
-    polylinePoints = [
-        [parseFloat(PreLa), parseFloat(PreLo)],
-        [parseFloat(Lati), parseFloat(Longi)] ];   
-    polyline = L.polyline(polylinePoints).addTo(map);     
-    console.log("Añadí: ", Lati, Longi)
-    } 
+    if(parseFloat(Lati)!=parseFloat(PreLa)&&parseFloat(Longi)!=parseFloat(PreLo)){
+        var marker = L.marker([parseFloat(Lati), parseFloat(Longi)]).addTo(map) //Añade marcadores        
+        //map.flyTo([parseFloat(Lati), parseFloat(Longi)])
+        polylinePoints = [
+            [parseFloat(PreLa), parseFloat(PreLo)],
+            [parseFloat(Lati), parseFloat(Longi)] ];   
+        polyline = L.polyline(polylinePoints).addTo(map);     
+        console.log("Añadí: ", Lati, Longi)
+        } 
+}
 PreLa=Lati;
 PreLo=Longi;
 }
