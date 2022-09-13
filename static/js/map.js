4bfd380277e570e3efabe09ee5baef9103d61f0c
Danielsaggar <yesid.p.g06@gmail.com>
2022-09-12 19:56:38 -0500

    Pruebas #1-PoliLinea

--- static/js/map.js
+++ static/js/map.js
@@ -1,4 +1,5 @@
 var map = L.map('map').setView([parseFloat(Lati), parseFloat(Longi)], 13);
+var polylinePoints;
 PreLa=0, PreLo=0;
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -12,6 +13,10 @@ if(parseFloat(Lati)!=parseFloat(PreLa)&&parseFloat(Longi)!=parseFloat(PreLo)){
     var marker = L.marker([parseFloat(Lati), parseFloat(Longi)]).addTo(map) //Añade marcadores
     //map.flyTo([parseFloat(Lati), parseFloat(Longi)])
     map.flyTo([parseFloat(Lati), parseFloat(Longi)])
+    polylinePoints = [
+        [parseFloat(PreLa), parseFloat(PreLo)],
+        [parseFloat(Lati), parseFloat(Longi)] ];   
+    var polyline = Wrld.polyline(polylinePoints).addTo(map);     
     console.log("Añadí: ", Lati, Longi)
     } 
 PreLa=Lati;
