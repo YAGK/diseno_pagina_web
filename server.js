812fb5a4f5f57948f2f144cfd2e8bbcf3d0ea405
KevinGg1008 <Kevinguegra@gmail.com>
2022-09-18 17:03:30 -0500

    Envio de datos directos

--- server.js
+++ server.js
@@ -6,19 +6,22 @@
 // ------------------------------
 //npm i pm2 -g
 //pm2 start server.js
-require('dotenv').config()
+//require('dotenv').config()
 const express = require('express')
 const app = express()
 const udp = require('dgram')
 const mysql = require('mysql')
-
+let lat 
+let long 
+let date 
+let time 
 app.listen(80, ()=>console.log('Mi servidor está corriendo sobre el puerto 80'))
 app.use(express.static(__dirname + "/static"));
 const connection = mysql.createConnection({
-    host: process.env.yagk_dns,
-    user: process.env.yagk_user,
-    password: process.env.yagk_pass,
-    database:process.env.yagk_data,
+    host: "disenoyagk.cuompzorqnem.us-east-1.rds.amazonaws.com",
+    user: "YAGK01",
+    password: "tobiascookiemaxtom",
+    database:'diseno',
     multipleStatements: true
     });
     connection.connect();
@@ -31,10 +34,11 @@ server.bind(52000)
 server.on('message',(data)=>{    
     let dataFormatted = data.toString('utf8')        
         var msj = dataFormatted.split('%');
-        let lat = msj[0]
-        let long = msj[1]
-        let date = msj[2]
-        let time = msj[3]
+        console.log(msj)
+        lat = msj[0]
+        long = msj[1]
+        date = msj[2]
+        time = msj[3]
         const query = "INSERT INTO datos (Latitud, Longitud, Fecha, Hora) VALUES (' "+ lat +"' , ' "+ 
         long +" ', ' "+ date+"', ' "+time+" ' ) ;"
         connection.query(query,(e)=>{
@@ -49,17 +53,10 @@ app.get('/',(req,res)=>{
 })
 
 app.get('/getData',(req,res)=>{
-
-    const query = 'SELECT * FROM datos ORDER BY ID DESC LIMIT 1'
-
-    connection.query(query,(e,data)=>{
-        if(e){
-            console.log(e)
-        }else{            
-            let datas = data
-            res.status(200).json({
-                data: datas
-            })
-        }
+    res.status(200).json({
+        lat:lat,
+        long:long,
+        date:date,
+        time:time
     })
 })
--- static/js/conect.js
+++ static/js/conect.js
@@ -1,19 +1,21 @@
-var Lati="11.015";
-var Longi="-74.8370";
+var Lati="10.96703";
+var Longi="-74.788";
 function act(){
     fetch('/getData')
     .then(res => res.json())
     .then(data => {        
         let str = [];
-        data.data.map(item => {
-            //console.log(Object.keys(item))
-            str[0]= `${item.Latitud} `
-            str[1]= `${item.Longitud} `
-            str[2]= `${item.Fecha}`
-            str[3]= `${item.Hora} `
-        });               
+        console.log("Esta es la data =",data)
+      // data.map(item => {
+        // console.log(Object.keys(item))
+            str[0]= data.lat
+            str[1]= data.long
+            str[2]= data.date
+            str[3]= data.time
+     //   });               
                Lati=str[0];
                Longi=str[1];
+
                str[2] = new Date(str[2])
                str[2] = str[2].getFullYear() +':'+(str[2].getMonth()+1).toString().padStart(2,'0') +':' + (str[2].getDate()+1).toString().padStart(2,'0')
                 document.getElementById("lat").innerText = str[0];
@@ -23,7 +25,6 @@ function act(){
     
     });
     }
-    //Activar ciclo
     act()
     setInterval(act, 4900)
     
