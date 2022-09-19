7054e6441440fec740a899cf72aede382e57a227
KevinGg1008 <Kevinguegra@gmail.com>
2022-09-19 00:22:28 -0500

    Historicos funcionando el post pendiente de polilinea

--- server.js
+++ server.js
@@ -10,82 +10,99 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const udp = require('dgram')
+const https = require('https');
 const mysql = require('mysql')
-
-let lat 
-let long 
-let date 
+const { reset } = require('nodemon')
+const bodyParser = require("body-parser")
+app.use(express.urlencoded())
+app.use(express.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+let lat
+let long
+let date
 let time
-app.listen(80, ()=>console.log('Mi servidor está corriendo sobre el puerto 80'))
+app.listen(80, () => console.log('Mi servidor está corriendo sobre el puerto 80'))
 app.use(express.static(__dirname + "/static"));
 const connection = mysql.createConnection({
     host: process.env.yagk_dns,
     user: process.env.yagk_user,
     password: process.env.yagk_pass,
-    database:process.env.yagk_data,
+    database: process.env.yagk_data,
     multipleStatements: true
-    });
-    connection.connect();
+});
+connection.connect();
 
 const server = udp.createSocket('udp4')
-server.on('listening',()=>{
+server.on('listening', () => {
     console.log('Server UDP ON')
 })
 server.bind(52000)
-server.on('message',(data)=>{    
-    let dataFormatted = data.toString('utf8')        
-        var msj = dataFormatted.split('%');
-        console.log(msj)
-         lat = msj[0]
-         long = msj[1]
-         date = msj[2]
-         time = msj[3]
-        const query = "INSERT INTO datos (Latitud, Longitud, Fecha, Hora) VALUES (' "+ lat +"' , ' "+ 
-        long +" ', ' "+ date+"', ' "+time+ " ' ) ;"
-        connection.query(query,(e)=>{
-            if(e){
-                console.log(e)
-            }
-        })
-})
-
-app.get('/',(req,res)=>{    
-    res.sendFile(__dirname+'/pagina_sobria.html')
-})
-
-
-
-app.get('/getData',(req,res)=>{
-    res.status(200).json({
-        lat:lat,
-        long:long,
-        date:date,
-        time:time
+server.on('message', (data) => {
+    let dataFormatted = data.toString('utf8')
+    var msj = dataFormatted.split('%');
+    console.log(msj)
+    lat = msj[0]
+    long = msj[1]
+    date = msj[2]
+    time = msj[3]
+    const query = "INSERT INTO datos (Latitud, Longitud, Fecha, Hora) VALUES (' " + lat + "' , ' " +
+        long + " ', ' " + date + "', ' " + time + " ' ) ;"
+    connection.query(query, (e) => {
+        if (e) {
+            console.log(e)
+        }
     })
 })
 
-app.get('/historicos',(req,res)=>{    
-    res.sendFile(__dirname+'/historicos.html')
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/pagina_sobria.html')
 })
 
-app.post("/Registro", (req, res) => {
-    var val =[
-        req.body.ini,
-        req.bodu.fin ]
-    initime=val[0]
-    fintime=val[1]
-    solquery= "SELECT Latitud, Longitud FROM datos WHERE timestamp(Fecha,Hora) between ' "+  
-    initime +"' and '"+fintime +"' "
-    connection.query(solquery,(e,data)=>{
-        if(e){
-            console.log(e)
-        } else {
-            res.status(200).json({
-                Latitud,
-                Longitud
-            })
-        }
-    })   
+
+
+app.get('/getData', (req, res) => {
+    res.status(200).json({
+        lat: lat,
+        long: long,
+        date: date,
+        time: time
+    })
+})
+
+app.get('/historicos', (req, res) => {
+    res.sendFile(__dirname + '/historicos.html')
+})
+
+
+app.post("/registro", (req, res) => {
+    console.log('Soy el post correcto', req.body)
+    try {
+        const initime = req.body.ini
+        const fintime = req.body.fin
+        solQuery = "SELECT Latitud, Longitud FROM datos WHERE timestamp(Fecha,Hora) between ' " +
+            initime + "' and '" + fintime + "' "
+        connection.query(solQuery, (e, data) => {
+            if (e) {
+                console.log(e)
+            } else if (data.length == 0) {
+                res.status(200).json({
+                    latitud: '0',
+                    longitud: '0'
+                })
+            }
+            else {
+                res.status(200).json({
+                    latitud: data.latitud,
+                    longitud: data.Longitud
+                })
+            }
+        })
+    }
+    catch (e) {
+        console.log(e)
+    }
 });
 
 
--- static/js/historicos.js
+++ static/js/historicos.js
@@ -1,35 +1,53 @@
-inicio=document.getElementById("dateAndTimePicker1").innerText;
-final=document.getElementById("dateAndTimePicker2").innerText;
 
-let info={ini:inicio.value,fin:final.value}
-fetch("/Registro", { //peticion http
-    method: 'POST', // tipo de peticion put get delete post , post manda informacion nueva , put actualiza algo existente 
-    body: JSON.stringify(info), // just in case 
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}).then(res => {
-    return res.json() //quitar cabeceras 
-}).then(Data=> {
+map = L.map('map').setView([parseFloat(Lati), parseFloat(Longi)], 16);
+marker = L.marker([parseFloat(Lati), parseFloat(Longi)]).addTo(map) //Añade marcadores  
 
-PreLa=Data[0].Latitud
-PreLo=Data[0].Longitud
-for (i=1;i<length(Data);i++){
-    Lati=Data[i].Latitud
-    Longi=Data[i].Longitud
-    if(parseFloat(PreLa)!=11.015&&parseFloat(PreLo)!=-74.8370){
-        map.flyTo([parseFloat(Lati), parseFloat(Longi)])
-        if(parseFloat(Lati)!=parseFloat(PreLa)&&parseFloat(Longi)!=parseFloat(PreLo)){
-            marker.setLatLng([parseFloat(Lati), parseFloat(Longi)])       
-            map.flyTo([parseFloat(Lati), parseFloat(Longi)])
-            polylinePoints = [
-                [parseFloat(PreLa), parseFloat(PreLo)],
-                [parseFloat(Lati), parseFloat(Longi)] ];   
-            polyline = L.polyline(polylinePoints).addTo(map);     
-            console.log("Añadí: ", Lati, Longi)
-            } 
+//ini = '2022-09-16 12:30:00'
+//fin = '2022-09-17 22:00:00'
+console.log("Se realizo los historicos inicio", inicio)
+
+async function historia() {
+
+    const inicio = document.getElementById("dateAndTimePicker1").value;
+    const final = document.getElementById("dateAndTimePicker2").value;
+    let headersList = {
+        "Accept": "*/*",
+        "Content-Type": "application/json"
     }
-    Prela=Lati
-    Prelo=Longi
+
+    let bodyContent = JSON.stringify({
+        "ini": inicio,
+        "fin": final
+    });
+
+    let response = await fetch("/registro", {
+        method: "POST",
+        body: bodyContent,
+        headers: headersList
+    }).then(res => {
+        return res.json() //quitar cabeceras 
+    }).then(Data => {
+        console.log(Data)
+
+        PreLa = Data[0].Latitud
+        PreLo = Data[0].Longitud
+        for (i = 1; i < length(Data); i++) {
+            Lati = Data[i].Latitud
+            Longi = Data[i].Longitud
+            if (parseFloat(PreLa) != 11.015 && parseFloat(PreLo) != -74.8370) {
+                map.flyTo([parseFloat(Lati), parseFloat(Longi)])
+                if (parseFloat(Lati) != parseFloat(PreLa) && parseFloat(Longi) != parseFloat(PreLo)) {
+                    marker.setLatLng([parseFloat(Lati), parseFloat(Longi)])
+                    map.flyTo([parseFloat(Lati), parseFloat(Longi)])
+                    polylinePoints = [
+                        [parseFloat(PreLa), parseFloat(PreLo)],
+                        [parseFloat(Lati), parseFloat(Longi)]];
+                    polyline = L.polyline(polylinePoints).addTo(map);
+                    console.log("Añadí: ", Lati, Longi)
+                }
+            }
+            Prela = Lati
+            Prelo = Longi
+        }
+    })
 }
-})
