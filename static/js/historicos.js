257d30bc1fcf81edb500fd8f95728bd52c6a3565
YesidP <yesid.p.g06@gmail.com>
2022-09-20 16:59:53 -0500

    Corrección polilinea

--- static/js/historicos.js
+++ static/js/historicos.js
@@ -19,29 +19,17 @@ async function historia() {
         return res.json() 
     }).then(data => {
         if (data.length != 0) {
-            let previousLat = 0
-            let previousLong = 0
             data = data.positions
-            for (const i in data) {
-                const currentLatitude = data[i].Latitud
-                const currentLongitud = data[i].Longitud
-                if (parseFloat(previousLat) != 11.015 && parseFloat(previousLong) != -74.8370) {
-                    map.flyTo([parseFloat(currentLatitude), parseFloat(currentLongitud)])
-                    if (parseFloat(currentLatitude) != parseFloat(previousLat) && parseFloat(currentLongitud) != parseFloat(previousLong)) {
-                        marker.setLatLng([parseFloat(currentLatitude), parseFloat(currentLongitud)])
-                        map.flyTo([parseFloat(currentLatitude), parseFloat(currentLongitud)])
-
-                        if (i > 1) {
-                            polylinePoints = [
-                                [parseFloat(previousLat), parseFloat(previousLong)],
-                                [parseFloat(currentLatitude), parseFloat(currentLongitud)]];
-                            polyline = L.polyline(polylinePoints).addTo(map);                     
-                        }
-                    }
-                }
-                previousLat = currentLatitude
-                previousLong = currentLongitud
-                
+            map.removeLayer(polyline);
+            polyline = L.polyline([]).addTo(map);
+            
+            for (const d of data) {
+                const currentLatitude = parseFloat(d.Latitud)
+                const currentLongitud = parseFloat(d.Longitud)
+                    map.flyTo([currentLatitude, currentLongitud])
+                    marker.setLatLng([currentLatitude, currentLongitud])
+                        map.flyTo([currentLatitude, currentLongitud])
+                        polyline.addLatLng([currentLatitude, currentLongitud])              
             }
         }
     })
