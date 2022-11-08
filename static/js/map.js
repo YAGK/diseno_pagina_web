cdfb28582590af2138419bf98056081ac19b265a
KevinGg1008 <Kevinguegra@gmail.com>
2022-11-08 14:11:01 -0500

    Cambios Pol2 k

--- static/js/map.js
+++ static/js/map.js
@@ -151,7 +151,7 @@ function Polimap(){
                 }
                 cam=0
             } else {
-                if(pol<2){
+                if(pol2<2){
                     polyline.addLatLng([parseFloat(rulat2[datos2-1]), parseFloat(rulon2[datos2-1])]) 
                     }  
                 }
