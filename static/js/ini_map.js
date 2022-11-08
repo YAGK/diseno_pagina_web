f8e21ae2c9fd97afd63cf9e6b2447d1132b8dd83
KevinGg1008 <Kevinguegra@gmail.com>
2022-11-08 15:44:29 -0500

    Cambios hist

--- static/js/ini_map.js
+++ static/js/ini_map.js
@@ -7,12 +7,9 @@ let iclupa = L.icon({
    
 });
 var map = L.map('map').setView([parseFloat(Lati), parseFloat(Longi)], 16);
-marker = L.marker([parseFloat(Lati), parseFloat(Longi)])
-marker2 = L.marker([parseFloat(Lati2), parseFloat(Longi2)])
+let marker = L.marker([parseFloat(Lati), parseFloat(Longi)])
 var polylinePoints = [];
-var polylinePoints2 = [];
 var polyline = L.polyline(polylinePoints).addTo(map);
-var polyline2 = L.polyline(polylinePoints2).addTo(map);
 var markerl = L.marker([parseFloat(Lati), parseFloat(Longi)], {icon: iclupa}); //Añade marcadores   
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
